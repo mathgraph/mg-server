54e235b52a6eeaa281bae54468baa6ce40b81591
mf-side <mf-side@mail.ru>
2015-09-21 22:17:00 +0300

    m

--- hooks/github-webhook-listener.js
+++ hooks/github-webhook-listener.js
@@ -7,30 +7,33 @@ var app = express();
 
 app.use(require('body-parser').json());
 
-app.post('/', function(req, res) {
-    var reqBody = req.body;
-    var branch = reqBody.ref.split('/')[2];
-    branch = branch || 'develop';
-    console.log('New push to ' + reqBody.ref + ' by ' + reqBody.pusher.name + ' (' + reqBody.pusher.email + ')');
+app.get('/', function(req, res) {
+//    var reqBody = req.body;
+//    var branch = reqBody.ref.split('/')[2];
+//    branch = branch || 'develop';
+//    console.log('New push to ' + reqBody.ref + ' by ' + reqBody.pusher.name + ' (' + reqBody.pusher.email + ')');
     // https://developer.github.com/webhooks/securing/
-    var hmac = crypto.createHmac('sha1', config.get('SECRET'));
+//    var hmac = crypto.createHmac('sha1', config.get('SECRET'));
     // http://stackoverflow.com/questions/12195480/node-js-crypto-cannot-create-hmac-on-chars-with-accents
     // http://stackoverflow.com/questions/9463157/nodejs-hmac-digest-issue-with-accents
-    hmac.update(new Buffer(JSON.stringify(reqBody)));
-    var signature = 'sha1=' + hmac.digest('hex');
-    if (signature !== req.headers['x-hub-signature']) {
-        console.log('Signatures didn\'t match!');
-        return res.status(500).send('Signatures didn\'t match!')
-    } else {
-        console.log('Execute hook.sh');
-        exec(__dirname + '/hook.sh ' + branch, function (error, stdout, stderr) {
+//    hmac.update(new Buffer(JSON.stringify(reqBody)));
+//    var signature = 'sha1=' + hmac.digest('hex');
+//    if (signature !== req.headers['x-hub-signature']) {
+//        console.log('Signatures didn\'t match!');
+//        return res.status(500).send('Signatures didn\'t match!')
+//    } else {
+//        console.log('Execute hook.sh');
+        exec('pm2 deploy ecosystem.json production', function (error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
             if (error !== null) {
+		exec('npm --prefix ./../  install', function(){});
+                exec('bower cash clear', function(){});
+                exec('bower install --config.cwd=./../', function(){}),  
                 console.log('exec error: ' + error);
             }
         });
-    }
+//    }
     return res.sendStatus(200);
 });
 
