df36000bd9a3d670d450e45729ece6f79817119a
mf-side <mf-side@mail.ru>
2015-09-21 20:06:02 +0300

    fix(hooks): hook.sh

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-branch="develop"
+branch="master"
 if [ "$1" != "" ]
 then
     branch=$1
 fi
-#git checkout $branch
-#git fetch
-#git reset --hard origin/$branch
+git checkout $branch
+git fetch
+git reset --hard origin/$branch
 npm install
 #npm run import
 #npm run front
+
 if [ "$branch" = "master" ]
 then
-    pm2 deploy /var/www/mg-server/ecosystem.json production mg-server
+    cd mg-main-prod
+    bower install
+    cd ..
+    pm2 startOrRestart ecosystem.json production
 else
-    pm2 deploy /var/www/mg-server/ecosystem.json develop mg-server
+    cd mg-main 
+    bower install
+    cd ..
+    pm2 startOrRestart ecosystem.json branch
 fi
-
