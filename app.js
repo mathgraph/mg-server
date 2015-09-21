f694105dc660fc55d52a854bf96d009457e93289
mf-side <mf-side@mail.ru>
2015-09-21 15:59:16 +0300

    fix(app.js): start production as default

--- app.js
+++ app.js
@@ -6,21 +6,18 @@ app
     .use(require('body-parser').urlencoded({ extended: false }))
     .use(require('body-parser').json());
 
-if (process.env.NODE_ENV == 'develop'){
+if (process.env.NODE_ENV == 'production'){
+    app
+        .use(express.static(__dirname + '/mg-main-prod'))
+        .listen(config.get('port_prod'), function(){
+            console.log('Instance running on port ', config.get('port_prod'));
+        });
+} else {
     app
         .use(require('morgan')('dev'))
         .use(express.static(__dirname + '/mg-main'))
         .listen(config.get('port_dev'), function(){
             console.log('Instance running on port ', config.get('port_dev'));
         });
-} else {
-    app
-        .use(express.static(__dirname + '/mg-main-prod'))
-        .use(express.static(__dirname + '/mg-main'))
-        .listen(config.get('port_prod'), function(){
-            console.log('Instance running on port ', config.get('port_prod'));
-        });
+
 }
-
-
-
