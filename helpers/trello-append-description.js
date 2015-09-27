83b8f77c229eaff3102eec6e894b429073100611
sivsais <sivsais@gmail.com>
2015-09-27 16:56:11 +0300

    fix(desc): value

--- helpers/trello-append-description.js
+++ helpers/trello-append-description.js
@@ -5,7 +5,7 @@ module.exports = function (idShort, comment) {
     trello.get('1/boards/' + board + '/cards', function (err, data) {
         data.forEach(function (card) {
             if (card.idShort === idShort) {
-                trello.put('1/cards/' + card.id + '/desc', { text: card.desc + '\n' + comment }, function (err, response) {
+                trello.put('1/cards/' + card.id + '/desc', { value: card.desc + '\n' + comment }, function (err, response) {
                     err && console.log(err);
                 });
             }
