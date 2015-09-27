1b572a07eeb252721aa0582f4cdfaa906d8ef0b6
sivsais <sivsais@gmail.com>
2015-09-27 14:01:56 +0300

    fix(trello): comment -> description

--- helpers/github-commits.js
+++ helpers/github-commits.js
@@ -1,9 +1,9 @@
-var comment = require('./trello-create-comment');
+var desc = require('./trello-append-description');
 var pattern = /^\[(\d+)\]/;
 
 module.exports = function (data) {
 	data.commits.forEach(function (commit) {
 		var tmp = pattern.exec(commit.message);
-		tmp && comment(+tmp[1], commit.message + ':\n' + commit.url);
+		tmp && desc(+tmp[1], commit.message + '\n' + commit.url + '\n');
 	});
 };
---
+++ helpers/trello-append-description.js
@@ -0,0 +1,14 @@
+var trello = require('./trello');
+var board = process.env.WEBHOOK_TRELLO_BOARD;
+
+module.exports = function (idShort, comment) {
+    trello.get('1/boards/' + board + '/cards', function (err, data) {
+        data.forEach(function (card) {
+            if (card.idShort === idShort) {
+                trello.put('1/cards/' + card.id + '/desc', { text: card.desc + '\n' + comment }, function (err, response) {
+                    err && console.log(err);
+                });
+            }
+        });
+    });
+};
